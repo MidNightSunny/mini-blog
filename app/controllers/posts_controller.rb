4e9caaa564b4b607f37e2f7ad330ed175cc5368c
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-06-26 19:18:41 +0900

    Set Content column
    Contentカラムにデータを保存できるように、form_forを設定。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+      redirect_to 'http://localhost:3000/'
+  end
+  
+  private
+   def post_params
+     params.require(:post).permit(:content).merge(user_id:current_user.id)
+   end
+
 end
