a870b7e9a54cc09743445b0618e9f5772f7b9ee4
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-06-26 15:21:43 +0900

    Update routes.rb
    サインアウト時にルートのエラーが出たので、deviseでスコープを設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
+  resources :posts do
+  end
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
 end
