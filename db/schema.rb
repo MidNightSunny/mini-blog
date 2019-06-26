6ff5837d35d50ff5bb186ddc61efcdb3b22acc45
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-06-26 16:21:28 +0900

    Change column name
    カラム名がpostでは分かりづらかったためcontentに変更しました。

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_06_24_115747) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "post"
+    t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
