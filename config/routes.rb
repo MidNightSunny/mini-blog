0e0bbe1429a7dd3e49d645a770ad6efbca50d7d4
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-06-26 19:16:25 +0900

    install pry-rails
    binding.pryをできるようにしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :posts do
+  resources :posts do 
   end
 
-  devise_scope :user do
+    devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  
 end
