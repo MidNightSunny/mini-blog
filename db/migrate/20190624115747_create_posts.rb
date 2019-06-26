3f78509de474bd0c4c71e178205b366bbe134710
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-06-26 12:10:32 +0900

    create post table
    投稿内容を管理するpostテーブルを作成。

--- db/migrate/20190624115747_create_posts.rb
+++ db/migrate/20190624115747_create_posts.rb
@@ -1,6 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.text :post
+      t.integer :user_id, foreign_key: true
 
       t.timestamps
     end
