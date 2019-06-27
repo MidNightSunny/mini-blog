6ef442155c0b401723bca1620fc53f1f22161954
MidNightSunny <52094306+MidNightSunny@users.noreply.github.com>
2019-06-27 12:59:42 +0900

    Update posts_controller.rb
    投稿した内容が中央表示されるようにMateriarizeで設定。投稿の削除機能のためにdestroyメソッドを追加。showは今の所使う予定がないので空にしてあります。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,29 @@
 class PostsController < ApplicationController
   
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user).order("created_at DESC")
   end
 
   def new
     @post = Post.new
   end
 
+  def show
+
+  end
+  
+
   def create
     Post.create(post_params)
       redirect_to 'http://localhost:3000/'
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.user_id == current_user.id
+       post.destroy 
+       redirect_to("http://localhost:3000/")
+    end
   end
   
   private
